177f72a29e4310c5f316db00969c9d37b6a93d67
Manoj-Bisht <Manoj.Kumar.Bisht@gmail.com>
2014-03-05 15:59:47 +0530

    Add phonecontroller sample class..

---
+++ Store.API/Controllers/PhonesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Store.API.Controllers
+{
+    public class PhonesController : ApiController
+    {
+        //GET: api/Phones
+        public IHttpActionResult Get()
+        {
+            return Ok();
+        }
+
+        //GET: api/Phones/1
+        public IHttpActionResult Get(string id)
+        {
+            return Ok();
+        }
+    }
+}
