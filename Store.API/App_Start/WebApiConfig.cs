3c0d542457f63148c1c7c5d884fac11fec208b70
Sumit Khandelwal <sumit.khandelwal@equinox.com>
2014-03-25 01:47:28 +0530

    Default Web Api Routing and Rpc Style Routing.

--- Store.API/App_Start/WebApiConfig.cs
+++ Store.API/App_Start/WebApiConfig.cs
@@ -18,9 +18,17 @@ namespace Store.API
             config.MapHttpAttributeRoutes();
 
             config.Routes.MapHttpRoute(
+                name: "RpcStyleApi",
+                routeTemplate: "rpc/{controller}/{action}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
+                //routeTemplate: "api/{orgid}/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
+                //, constraints: new { orgid = @"\d+" }
             );
         }
     }
--- Store.API/Controllers/PhonesController.cs
+++ Store.API/Controllers/PhonesController.cs
@@ -12,13 +12,36 @@ namespace Store.API.Controllers
 {
     public class PhonesController : ApiController
     {
+        static readonly IPhoneRepository<Phone> repository = new PhoneRepository();
 
-        static readonly IPhoneRepository<Phone> repository = new PhoneRepository(); 
+        public PhonesController()
+        {
+            repository.Add(new Phone()
+            {
+                Id = "samsung-gem",
+                Age = 30,
+                Name = "Samsung"
+            });
+            repository.Add(new Phone()
+            {
+                Id = "Nokia-gem",
+                Age = 40,
+                Name = "Nokia"
+            }); 
+            repository.Add(new Phone()
+            {
+                Id = "micromax-gem",
+                Age = 10,
+                Name = "Micromax"
+            });
+        }
+
+        #region Default Routing with Action Names verb
 
         //GET: api/Phones
         public IHttpActionResult Get()
         {
-            var items = repository.GetAll();            
+            var items = repository.GetAll();
             return Ok(items);
         }
 
@@ -29,7 +52,7 @@ namespace Store.API.Controllers
             if (item == null)
             {
                 //var err = new HttpError(string.Format("Phone with id = {0} not found.", id));
-                return NotFound();                 
+                return NotFound();
             }
             else
             {
@@ -37,10 +60,71 @@ namespace Store.API.Controllers
             }
         }
 
+        //PUT: api/Phones/samsung-gem
+        //Request Body:
+        //{'age':30,'Name':'Samsung-Gem'}
+        public IHttpActionResult Put(string id,Phone phone)
+        {
+            var item = repository.Get(id);
+            if (item == null)
+            {
+                //var err = new HttpError(string.Format("Phone with id = {0} not found.", id));
+                return NotFound();
+            }
+            else
+            {
+                item.Age = phone.Age;
+                var result = repository.Update(item);
+                return Ok(result);
+            }
+        }
+
+        //POST: api/Phones/
+        public IHttpActionResult Post(Phone phone)
+        {
+            var result = repository.Add(phone);
+            return Ok(result);
+        }
+
         //DELETE: api/Phones/samsung-gem
         public IHttpActionResult Delete(string id)
         {
+            repository.Remove(id);
             return Ok();
         }
+
+        #endregion
+
+        #region Default Routing 2 with ActionNames suffix
+        ////GET: api/Phones
+        //public IHttpActionResult GetPhones()
+        //{
+        //    var items = repository.GetAll();
+        //    return Ok(items);
+        //}
+
+        ////GET: api/Phones/samsung-gem
+        //public IHttpActionResult GetPhones(string id)
+        //{
+        //    var item = repository.Get(id);
+        //    if (item == null)
+        //    {
+        //        //var err = new HttpError(string.Format("Phone with id = {0} not found.", id));
+        //        return NotFound();
+        //    }
+        //    else
+        //    {
+        //        return Ok(item);
+        //    }
+        //}
+
+        ////DELETE: api/Phones/samsung-gem
+        //public IHttpActionResult DeletePhone(string id)
+        //{
+        //    return Ok();
+        //}
+
+        #endregion
+
     }
 }
